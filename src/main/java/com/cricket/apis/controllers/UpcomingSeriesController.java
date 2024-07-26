21a5829214758268f339cb92cb632562c1be5698
Sarvech <sarwech99@gmail.com>
2024-07-26 21:57:02 +0500

    add Cross Origin annotation for frontend calling api.

--- src/main/java/com/cricket/apis/controllers/UpcomingSeriesController.java
+++ src/main/java/com/cricket/apis/controllers/UpcomingSeriesController.java
@@ -4,12 +4,14 @@ import com.cricket.apis.helper.UpcomingSeriesResponse;
 import com.cricket.apis.service.UpcomingSeriesService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/upcoming-matches")
+@CrossOrigin("*")
 public class UpcomingSeriesController {
 
     private UpcomingSeriesService upcomingSeriesService;
